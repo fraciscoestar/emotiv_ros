5ea8eec5139c1041de9ba7ab9e9325bfcb9dbc89
Francisco J Garrido <fragarflo2@alum.us.es>
2022-01-10 16:34:32 +0100

    Initial version

---
+++ scripts/command.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+from cortex import Cortex
+from emotiv_mc.msg import MentalCommand
+import rospy
+from std_msgs import msg
+
+user = {
+    "license" : "",
+    "client_id" : "RrsVsnaGuPCnzOaXrvqCZkcDW40BabiLjQlCoudD",
+    "client_secret" : "HU9nA1ScWfsOGmqDsy1Kaoo1B3PRMjrnyFVfwlLvkGCelZeaiI8UOW9cqRciWQKsbhAkzoRGDkqsvR0EtlCHV5Bxt7h4HwbzPsWcfgar3S76b7qyoFxk9amMVuYepEfm",
+    "debit" : 100
+}
+
+# name of profile
+profile_name = 'samue'
+
+class Command():
+
+    def __init__(self):
+        self.c = Cortex(user, debug_mode=True)
+        self.c.bind(new_com_data=self.on_new_data)
+
+    def do_prepare_steps(self):
+        self.c.do_prepare_steps()
+
+    def subscribe_data(self, streams):
+        # streams : list, required list of streams. For example, ['com']
+        self.c.sub_request(streams)
+
+    def load_profile(self, profile_name):
+        profiles = self.c.query_profile()
+
+        if profile_name not in profiles:
+            status = 'create'
+            self.c.setup_profile(profile_name, status)
+
+        status = 'load'
+        self.c.setup_profile(profile_name, status)
+
+    def unload_profile(self, profile_name):
+        profiles = self.c.query_profile()
+
+        if profile_name in profiles:
+            status = 'unload'
+            self.c.setup_profile(profile_name, status)
+        else:
+            print("The profile " + profile_name + " does not exist.")
+
+    def on_new_data(self, *args, **kwargs):
+        data = kwargs.get('data')
+        # print(data["action"])
+        msg = MentalCommand()
+        msg.command = data["action"]
+        msg.strength = data["power"]
+
+        try:
+            publish(msg)
+        except rospy.ROSInterruptException: pass
+
+#######################################################################
+
+def publish(data):
+        pub = rospy.Publisher('/mental_command', MentalCommand, queue_size=32)
+
+        if not rospy.is_shutdown():
+            pub.publish(data)
+
+if __name__ == '__main__':
+
+    rospy.init_node('emotiv_server', anonymous=True)
+
+    # Init Command
+    t=Command()
+
+    # Do prepare steps
+    t.do_prepare_steps()
+
+    # load existed profile or create a new profile
+    t.load_profile(profile_name)
+
+    # subscribe sys stream to receive Training Event
+    t.subscribe_data(['com'])
+
+    # unload profile
+    t.unload_profile(profile_name)
+
+# -----------------------------------------------------------
---
+++ src/emotiv_client.cpp
@@ -0,0 +1,59 @@
+#include "ros/ros.h"
+#include "emotiv_ros/MentalCommand.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
+#include <stdio.h>
+
+float maxLinearSpeed;
+float maxAngularSpeed;
+
+const std::string robotName = "turtlebot";
+ros::Publisher publisher;
+
+void MentalCommandCallback(const emotiv_ros::MentalCommand _command)
+{
+    geometry_msgs::Twist velocity;
+
+    if (_command.command == "push")
+    {
+        velocity.linear.x = _command.strength * maxLinearSpeed;
+        velocity.angular.z = 0;
+    }
+    else if (_command.command == "pull")
+    {
+        velocity.linear.x = -_command.strength * maxLinearSpeed;
+        velocity.angular.z = 0;
+    }
+    else if (_command.command == "left")
+    {
+        velocity.linear.x = 0;
+        velocity.angular.z = _command.strength * maxAngularSpeed;
+    }
+    else if (_command.command == "right")
+    {
+        velocity.linear.x = 0;
+        velocity.angular.z = -_command.strength * maxAngularSpeed;
+    }
+    else
+    {
+        velocity.linear.x = 0;
+        velocity.angular.z = 0;
+    }
+
+    publisher.publish(velocity);
+}
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "emotiv_client");
+    ros::NodeHandle nh;
+
+    nh.getParam("/max_linear_speed", maxLinearSpeed);
+    nh.getParam("/max_angular_speed", maxAngularSpeed);
+
+    ros::Subscriber commandSubscriber = nh.subscribe<emotiv_ros::MentalCommand>("mental_command", 100, MentalCommandCallback);
+    publisher = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 100);
+
+    ros::spin();
+    return 0;
+}
